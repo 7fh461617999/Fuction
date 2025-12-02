7cf37637fbeed7742deb80fa7e1234c415425023
habibi <habibifirman117@gmail.com>
2025-12-02 08:12:05 +0700

    Commit 1

---
+++ Jobsheet12/Gratitude_11.java
@@ -0,0 +1,11 @@
+package Jobsheet12;
+
+public class Gratitude_11 {
+    public static void main(String[] args) {
+        sayThankyou();
+    }
+    public static void sayThankyou() {
+        System.out.println("Thank you for being the best teacher in the world.\n"+ 
+        "You inspired in me a love for learning and made me feel like I could ask you anything.");
+    }
+}
---
+++ Jobsheet12/QuestionAnswer.md
@@ -0,0 +1,5 @@
+## question answer in jobsheet 12
+### Experiment 1 (Gratitude_11.java)
+1. This keyword means the function does not return a value. It does its job and finishes.
+2. It is posible. We can sout this santance without call the other funtion 
+3. 
